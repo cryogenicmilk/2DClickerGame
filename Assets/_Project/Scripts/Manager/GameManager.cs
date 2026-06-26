2e79a2fc03f070a52981bb2371e93e7776394559
藤井崚太 <168154201+cryogenicmilk@users.noreply.github.com>
2026-06-26 21:31:57 +0900

    アップグレードUIの見た目作成

--- Assets/_Project/Scripts/Manager/GameManager.cs
+++ Assets/_Project/Scripts/Manager/GameManager.cs
@@ -5,7 +5,7 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        AudioPlayer.Instance.PlayBGM(0);
     }
 
     // Update is called once per frame
